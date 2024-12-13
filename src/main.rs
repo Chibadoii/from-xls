e8cdb41bd36d4c5e7d266c1e82e05e9518723926
Chibadoii <ghostrider.max@mail.ru>
2024-12-13 17:02:12 +0300

    before sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
+mod dto;
+//use sqlx::Connection;
 use calamine::{
-    open_workbook, DataType, DeError, Range, RangeDeserializerBuilder, Reader, Xls, Xlsx,
+    open_workbook, DataType, DeError, Range, RangeDeserializer, RangeDeserializerBuilder, Reader,
+    Xls, Xlsx,
 };
 use serde::{Deserialize, Serialize};
-use std::ffi::OsStr;
+use dto::TestStruct;
 use std::path::Path;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct TestStruct {
-    #[serde(default)]
-    column1: Option<String>,
-    #[serde(default)]
-    column2: Option<String>,
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let filename = std::env::args().skip(1).collect::<Vec<_>>();
     let path = Path::new(&filename[0]);
     let mut workbook: Xls<_> = open_workbook(path)?;
@@ -22,19 +18,37 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .worksheet_range("Sheet1")
         .ok_or(calamine::Error::Msg("Cannot find 'Sheet1'"))??;
 
-    processing(path.file_name().unwrap(), range)
+    //let pg_pool = sqlx::PgPool::connect("postgresql://postgres:postgres@localhost/library").await?;
+    let _res = processing_example(path.file_name().unwrap().to_str().unwrap(), &range);
+    processing(path.file_name().unwrap().to_str().unwrap(), &range)
 }
 
-fn processing(filename: &OsStr, range: Range<DataType>) -> Result<(), Box<dyn std::error::Error>> {
-    let mut iter = RangeDeserializerBuilder::new()
+fn processing_example(
+    filename: &str,
+    range: &Range<DataType>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut iter: RangeDeserializer<DataType, Vec<String>> = RangeDeserializerBuilder::new()
         .has_headers(false)
         .from_range(&range)?;
 
     if let Some(result) = iter.next() {
         let storage: Vec<String> = result?;
         for item in storage {
-            println!("{:?}", item);
+            dbg!(item);
         }
+    }
+
+    Ok(())
+}
+
+fn processing(filename: &str, range: &Range<DataType>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut iter: RangeDeserializer<DataType, Vec<String>> = RangeDeserializerBuilder::new()
+        .has_headers(false)
+        .from_range(range)?;
+
+    match filename {
+        "test.xls" => {println!("{:?}", TestStruct::new(iter))},
+        _ => {}
     }
 
     Ok(())
