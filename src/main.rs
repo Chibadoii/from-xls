2c97aad69018ad3acafab470ccf3b073acd4f5d9
Chibadoii <ghostrider.max@mail.ru>
2024-12-13 11:20:43 +0300

    work example

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,16 @@
+use calamine::{
+    open_workbook, DataType, DeError, Range, RangeDeserializerBuilder, Reader, Xls, Xlsx,
+};
+use serde::{Deserialize, Serialize};
 use std::ffi::OsStr;
 use std::path::Path;
-use calamine::{open_workbook, DataType, DeError, Range, RangeDeserializerBuilder, Reader, Xls, Xlsx};
-use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct TestStruct{
+struct TestStruct {
     #[serde(default)]
     column1: Option<String>,
     #[serde(default)]
     column2: Option<String>,
-}
-
-impl TestStruct {
-    fn new (column1: String, column2: String) -> Self {
-        Self{
-            column1,
-            column2,
-        }
-    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -25,30 +18,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = Path::new(&filename[0]);
     let mut workbook: Xls<_> = open_workbook(path)?;
 
-    let range = workbook.worksheet_range("Sheet1")
+    let range = workbook
+        .worksheet_range("Sheet1")
         .ok_or(calamine::Error::Msg("Cannot find 'Sheet1'"))??;
 
-    processing(path.file_name().unwrap(), range);
+    processing(path.file_name().unwrap(), range)
 }
 
-fn processing(filename: &OsStr , range: Range<DataType>) -> Result<(), Box<dyn std::error::Error>>{
-    let mut iter = RangeDeserializerBuilder::new().has_headers(false).from_range(&range)?;
-
-    match filename.to_str().unwrap() {
-        "test" => {},
-        _ => {}
-    }
+fn processing(filename: &OsStr, range: Range<DataType>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut iter = RangeDeserializerBuilder::new()
+        .has_headers(false)
+        .from_range(&range)?;
 
     if let Some(result) = iter.next() {
-        let _ = processing(result);
-        Ok(())
-    } else {
-        Err(From::from("expected at least one record but got none"))
+        let storage: Vec<String> = result?;
+        for item in storage {
+            println!("{:?}", item);
+        }
     }
 
-    let storage: Vec<String> = value?;
-    for item in storage {
-        println!("{:?}", item);
-    }
     Ok(())
 }
